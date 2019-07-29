2730d5e83202061cc7daeca45b58b25dbc19e8dc
MaxPlays <maxplays88@gmail.com>
2019-07-29 23:07:18 +0200

    Added foreign keys

--- sql.php
+++ sql.php
@@ -10,6 +10,6 @@
   }
   $conn->query("CREATE TABLE IF NOT EXISTS users(userid INT AUTO_INCREMENT PRIMARY KEY, token VARCHAR(36), first TEXT, last VARCHAR(3), admin INT DEFAULT 0);");
   $conn->query("CREATE TABLE IF NOT EXISTS events(eventid INT AUTO_INCREMENT PRIMARY KEY, title TEXT, info TEXT, start LONG, end LONG, time TEXT, location TEXT, maps TEXT, priority INT DEFAULT 0);");
-  $conn->query("CREATE TABLE IF NOT EXISTS zusagen(userid INT, eventid INT, PRIMARY KEY(userid, eventid));");
-  $conn->query("CREATE TABLE IF NOT EXISTS absagen(userid INT, eventid INT, PRIMARY KEY(userid, eventid));");
+  $conn->query("CREATE TABLE IF NOT EXISTS zusagen(userid INT, eventid INT, PRIMARY KEY(userid, eventid), FOREIGN KEY (eventid) REFERENCES events (eventid) ON DELETE CASCADE, FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE);");
+  $conn->query("CREATE TABLE IF NOT EXISTS absagen(userid INT, eventid INT, PRIMARY KEY(userid, eventid), FOREIGN KEY (eventid) REFERENCES events (eventid) ON DELETE CASCADE, FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE);");
  ?>
